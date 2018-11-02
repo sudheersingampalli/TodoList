5e542ca81625f1543382f6f4227de17e5cdd77d0
Sudheer <singampallisudheer@gmail.com>
2018-11-02 18:17:27 +0900

    Debug is made false

--- Todo/settings.py
+++ Todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$!7y@2xhi3=sm6p)+-ij_&q1i+fmu73!f_-a%)w@sxks_yhhlb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']
 
