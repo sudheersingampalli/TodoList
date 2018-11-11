86016c051778d6f10da7a0a6584411900578b2fe
Sudheer <singampallisudheer@gmail.com>
2018-11-11 12:22:09 +0900

    date change from date.today() to date.today

--- Todoapp/models.py
+++ Todoapp/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 class Todomodel(models.Model):
 	CHOICES_STATUS = (('1', 'Pending',), ('2', 'Done',),('3','Not Required'),('4','Delete'))
 	
-	date = models.DateField(default = date.today()) #date.today()
+	date = models.DateField(default = date.today) #date.today()
 	description = models.TextField(max_length = 500,blank=False)
 	status = models.CharField(choices=CHOICES_STATUS,max_length=10,default='Pending')
 	employee_num = models.ForeignKey(User,on_delete=models.CASCADE)
